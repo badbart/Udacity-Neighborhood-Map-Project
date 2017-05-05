0f8eff1d00f2f17bc44418a3edf4d7dd72facba2
Tom <tom.zille@gmail.com>
2017-05-05 14:05:08 +0200

    Added Knockout, some cosmetical stuff

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
+'use strict';
 
+/**
+ * Is called from Button Click Event
+ */
+function clickButton() {
+  this.classList.toggle('is-active');
+}
+
+document.getElementById('button').addEventListener('click', clickButton);
