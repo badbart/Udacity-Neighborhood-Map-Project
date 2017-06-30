cbd2e0037f0def2894b7b7d82581ae0d93ed4c16
Tom <tom.zille@gmail.com>
2017-06-30 07:01:22 +0200

    Added better and more data to the locations

--- src/js/model.js
+++ src/js/model.js
@@ -4,36 +4,41 @@ var locations = [
   {
     id: 1,
     name: "Tatie",
-    lat: 50.108543,
-    lng: 8.663262,
-    highlight: "false"
+    lat: 50.108492,
+    lng: 8.663232,
+    highlight: "false",
+    address: "Am Hauptbahnhof 18, 60329 Frankfurt am Main"
   },
   {
     id: 2,
     name: "Palms Garden",
-    lat: 50.108377,
-    lng: 8.664179,
-    highlight: "false"
+    lat: 50.108354,
+    lng: 8.664235,
+    highlight: "false",
+    address: "Am Hauptbahnhof 16, 60329 Frankfurt am Main"
   },
   {
     id: 3,
     name: "Starbucks",
-    lat: 50.107567,
-    lng: 8.665938,
-    highlight: "false"
+    lat: 50.107603,
+    lng: 8.665878,
+    highlight: "false",
+    address: "Kaiserstraße 75-77, 60329 Frankfurt am Main"
   },
   {
     id: 4,
     name: "Nefis Kebap Haus",
-    lat: 50.106886,
-    lng: 8.66638,
-    highlight: "false"
+    lat: 50.106919,
+    lng: 8.66604,
+    highlight: "false",
+    address: "Münchener Str. 51, 60329 Frankfurt am Main"
   },
   {
     id: 5,
     name: "Jade Magic Wok",
-    lat: 50.107516,
-    lng: 8.667212,
-    highlight: "false"
+    lat: 50.107547,
+    lng: 8.66714,
+    highlight: "false",
+    address: "Moselstraße 25, 60329 Frankfurt am Main"
   }
 ];
