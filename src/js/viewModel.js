cff02e6179bbda9a55769ae34c49b2a37ddd7ff1
Tom <tom.zille@gmail.com>
2017-07-03 09:56:09 +0200

    Added the Filter Functionality

--- src/js/viewModel.js
+++ src/js/viewModel.js
@@ -8,10 +8,25 @@ function AppViewModel() {
   let self = this;
 
   /**
-   * The Markers for the Map
-   * @type {KnockoutObservableArray<T>}
+   * @type {KnockoutObservable<T>} The query string the user is typing in the search box
    */
-  self.markers = ko.observableArray(locations);
+  self.query = ko.observable("");
+
+  // noinspection JSValidateJSDoc
+  /**
+   * @type {KnockoutDependentObservable<T>} The Locations to show on the Map, using query for a Filter
+   */
+  self.locations = ko.dependentObservable(function() {
+    let search = self.query().toLowerCase();
+    return ko.utils.arrayFilter(locations, function(location) {
+      if (location.name.toLowerCase().indexOf(search) >= 0) {
+        showMarker(location);
+        return true;
+      }
+      hideMarker(location);
+      return false;
+    });
+  }, this);
 
   /**
    * Is the Modal Active at the Moment
@@ -31,6 +46,15 @@ function AppViewModel() {
    */
   self.deactivateModal = function() {
     self.modalActive(false);
+  };
+
+  /**   *
+   * @param {object} item The Location Object of the clicked Item
+   * Invokes the Actions necessary to show the InfoWindow of the clicked Item
+   */
+  self.showInfo = function(item) {
+    openInfoWindowOfMarker(item.marker);
+    self.deactivateModal();
   };
 
   /**
